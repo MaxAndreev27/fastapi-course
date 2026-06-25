014cb630587f716d7ee00b444149c24864f381b7
max <maxandreev27@gmail.com>
2026-06-25 15:00:15 +0300

    Move code from main to root router

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import auth, heroes, users
+from app.api.v1.endpoints import auth, heroes, root, users
 
 v1_router = APIRouter()
 
+v1_router.include_router(root.router, tags=["System"])
 v1_router.include_router(auth.router, prefix="/auth", tags=["Authentication V1"])
-v1_router.include_router(heroes.router, prefix="/heroes", tags=["Heroes V1"])
 v1_router.include_router(users.router, prefix="/users", tags=["Users V1"])
+v1_router.include_router(heroes.router, prefix="/heroes", tags=["Heroes V1"])
--- app/main.py
+++ app/main.py
@@ -1,11 +1,9 @@
 from contextlib import asynccontextmanager
-from typing import Annotated
 
-from fastapi import FastAPI, Path, Query, status
+from fastapi import FastAPI
 from fastapi.routing import APIRoute
-from pydantic import BaseModel
 
-from app.api.dependencies import SettingsDep, get_settings
+from app.api.dependencies import get_settings
 from app.api.v1.router import v1_router
 from app.core.database import init_db
 
@@ -30,38 +28,3 @@ app = FastAPI(
 )
 
 app.include_router(v1_router, prefix="/api/v1")
-
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: bool | None = None
-
-
-@app.get("/")
-async def root():
-    return {"Hello World"}
-
-
-@app.get("/items/{item_id}")
-async def read_item(
-    item_id: Annotated[int, Path(title="The ID of the item to get")],
-    q: Annotated[str | None, Query(max_length=50)] = None,
-):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/items/{item_id}", status_code=status.HTTP_201_CREATED)
-def update_item(item_id: int, item: Item) -> Item:
-    # return {"item_name": item.name, "item_id": item_id}
-    return item
-
-
-@app.get("/info")
-async def get_info(settings: SettingsDep):
-    return {
-        "project_name": settings.PROJECT_NAME,
-        "debug": settings.DEBUG,
-        "api_port": settings.API_PORT,
-        "database_url": settings.DATABASE_URL,
-    }

599092fba103067e35c7d1d4ebb85369dbc309f9
max <maxandreev27@gmail.com>
2026-07-15 18:49:59 +0300

    Add CORSMiddleware

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.routing import APIRoute
 
 from app.api.dependencies import get_settings
@@ -26,6 +27,19 @@ app = FastAPI(
     title=get_settings().PROJECT_NAME,
     lifespan=lifespan,
     generate_unique_id_function=custom_generate_unique_id,
+)
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Дозволяємо запити з цих джерел
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(v1_router, prefix="/api/v1")
